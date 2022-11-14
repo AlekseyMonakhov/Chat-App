b59a1a214a80d9dfe9d0fc27d48717a211241d4b
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-11-14 23:54:31 +0200

    upd

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
-const firebaseConfig = {
-  apiKey: "AIzaSyCfoWmbY5WjRhzUo3X83YO8ZaQS4gcYq3Q",
-  authDomain: "chat-9db38.firebaseapp.com",
-  projectId: "chat-9db38",
-  storageBucket: "chat-9db38.appspot.com",
-  messagingSenderId: "840236184819",
-  appId: "1:840236184819:web:c370cd619d3b3d70f2f386",
-};
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
